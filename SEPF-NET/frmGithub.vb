ad9c07cd73e570634d5eeb01b2162408341e230f
Jun <jyang@bacweb.org>
2019-06-06 16:14:26 -0400

    add new frm and cls for github test

---
+++ SEPF-NET/frmGithub.vb
@@ -0,0 +1,5 @@
+﻿Public Class frmGithub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("hellow Github")
+    End Sub
+End Class
---
+++ SEPF-NET/githubfrm.vb
@@ -0,0 +1,3 @@
+﻿Public Class githubfrm
+
+End Class
